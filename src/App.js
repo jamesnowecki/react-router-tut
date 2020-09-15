7b348fa1a8aeb43d2c9569f47159c56f6200f9a5
James Nowecki <james.nowecki@minstersoft.co.uk>
2020-09-15 09:45:14 +0100

    basic wrapping, pathing and exact pathing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./Nav";
+import About from "./About";
+import Shop from "./Shop";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div >
+        <Nav />
+        <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
+}
+
+const Home = ( ) => {
+  return <div>Home</div>
 }
 
 export default App;
---
+++ src/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const Nav = () => {
+  return (
+    <nav >
+      <h3>Log</h3>
+      <ul className="nav-links">
+          <li>About</li>
+          <li>Shop</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
